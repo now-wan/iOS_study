e3604fc7424a157742ffad696e935f911a725840
now-wan <from.nowwan@gmail.com>
2022-11-18 20:33:06 +0900

    StockRank

---
+++ StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 이제완 on 2022/11/18.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
