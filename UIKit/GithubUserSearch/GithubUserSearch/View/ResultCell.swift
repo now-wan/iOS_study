66ce052bf1b5ff89de16657e1d8d02c2249d052e
now-wan <from.nowwan@gmail.com>
2022-12-21 15:27:53 +0900

    Update Project

---
+++ UIKit/GithubUserSearch/GithubUserSearch/View/ResultCell.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GithubUserSearch
+//
+//  Created by 이제완 on 2022/12/21.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    
+    @IBOutlet weak var user: UILabel!
+}
+
--- UIKit/GithubUserSearch/GithubUserSearch/View/ResultCellswift.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ResultCellswift.swift
-//  GithubUserSearch
-//
-//  Created by 이제완 on 2022/12/20.
-//
-
-import Foundation
