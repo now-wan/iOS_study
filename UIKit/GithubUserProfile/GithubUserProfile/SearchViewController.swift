c592286d959f1328ffa52e9764d7327f3be61b6a
now-wan <from.nowwan@gmail.com>
2023-01-14 18:15:45 +0900

    Update Project

--- UIKit/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ UIKit/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -6,3 +6,18 @@
 //
 
 import UIKit
+import Combine
+import Kingfisher
+
+class UserProfileViewController: UIViewController {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
